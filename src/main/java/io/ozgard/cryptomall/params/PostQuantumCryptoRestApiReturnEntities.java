5831033becff73c1a022d0aaa9eb4a683c57dae0
Telefu21 <zgrrdnr@gmail.com>
2025-07-17 23:06:09 +0100

    PQC KEM Key Pair generate REST APIs implemented - new file added

---
+++ src/main/java/io/ozgard/cryptomall/params/PostQuantumCryptoRestApiReturnEntities.java
@@ -0,0 +1,27 @@
+package io.ozgard.cryptomall.params;
+
+public class PostQuantumCryptoRestApiReturnEntities 
+{
+	byte[] privateKey;
+	byte[] publicKey;
+	
+	public byte[] getPrivateKey() 
+	{
+		return privateKey;
+	}
+	
+	public void setPrivateKey(byte[] privateKey) 
+	{
+		this.privateKey = privateKey;
+	}
+	
+	public byte[] getPublicKey() 
+	{
+		return publicKey;
+	}
+	
+	public void setPublicKey(byte[] publicKey) 
+	{
+		this.publicKey = publicKey;
+	}
+}
